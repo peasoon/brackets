8d30bad34ccf1eca4aa46379ed7c5c95e9a7b622
peasoon <vasyarichman@mail.ru>
2022-02-05 16:40:55 +0300

    something

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+module.exports = function check (str, bracketsConfig) {
+  if (str.length % 2 !== 0) {
+    return false;
+  }
+  while (
+    str.includes("{}") ||
+    str.includes("()") ||
+    str.includes("[]") ||
+    str.includes("<>") ||
+    str.includes("||")
+  ) {
+    str = str
+      .replace("{}", "")
+      .replace("()", "")
+      .replace("[]", "")
+      .replace("<>", "")
+      .replace("||", "");
+  }
+
+  return str === "";
+};
